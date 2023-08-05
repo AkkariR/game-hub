238e633bb8b24ac9b28385071de8d8f24d668670
Ricardo Akkari <rakk6881@uni.sydney.edu.au>
2023-08-05 13:20:56 +1000

    Ship genres and platform with the app

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,9 +15,7 @@ export interface Game {
     parent_platforms: { platform: Platform }[];
     metacritic: number;
     rating_top: number;
-  }
-  
-  
+  }  
 
 const useGames = (gameQuery: GameQuery) => 
   useData<Game>('/games', { 
