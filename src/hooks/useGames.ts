4097a3c783dfd48d88c186d5592111a3d69682fb
Ricardo Akkari <rakk6881@uni.sydney.edu.au>
2023-08-07 18:15:26 +1000

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,7 +22,8 @@ useInfiniteQuery<FetchResponse<Game>, Error>({
   }),
   getNextPageParam: (lastPage, allPages) => {
     return lastPage.next ? allPages.length + 1 : undefined;
-  }
+  },
+  staleTime: 24 * 60 * 60 * 1000 // 24hrs
 });
 
 export default useGames;
