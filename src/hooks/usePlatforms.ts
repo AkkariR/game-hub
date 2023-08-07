9b1c9569b706781aff74163348b77ac881c40b22
Ricardo Akkari <rakk6881@uni.sydney.edu.au>
2023-08-07 17:45:39 +1000

    Refactoring: fetching using react query

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,20 @@
+import platforms from "../data/platforms";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
+
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+const usePlatforms = () => useQuery({
+  queryKey: ['pltaforms'],
+  queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents').then(res => res.data),
+  staleTime: 24 * 60 * 60 * 1000, // 24hrs
+  initialData: {count: platforms.length, results: platforms}
+  
+})
+
+export default usePlatforms;
